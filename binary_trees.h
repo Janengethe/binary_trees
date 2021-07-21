e786d306e35b0605430723210623fa74c351ed00
Janengethe <janengethew@gmail.com>
2021-07-21 02:54:01 -0700

    task 0

--- binary_trees.h
+++ binary_trees.h
@@ -2,6 +2,7 @@
 #define _BINARY_TREES_H_
 
 #include <stdlib.h>
+#include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -31,6 +32,8 @@ typedef struct binary_tree_s avl_t;
 
 /*Max Binary Heap*/
 typedef struct binary_tree_s heap_t;
+
+void binary_tree_print(const binary_tree_t *);
 
 /*Tasks prototypes*/
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
