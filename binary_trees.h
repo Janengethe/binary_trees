74d2f4114af3da4af2516ddd3de0d0e94de12698
Janengethe <janengethew@gmail.com>
2021-07-21 02:05:27 -0700

    betty

--- binary_trees.h
+++ binary_trees.h
@@ -13,10 +13,11 @@
  */
 struct binary_tree_s
 {
-  int n;
-  struct binary_tree_s *parent;
-  struct binary_tree_s *left;
-  struct binary_tree_s *right;
+	int n;
+
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 /*Basic Binary Tree*/
